048d54ddb6d71444abf79b98e763d949e7f79ca4
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-06-11 12:23:28 -0600

    Challenges completed

--- Day41/hello.py
+++ Day41/hello.py
@@ -9,6 +9,10 @@ print(random.__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/bye")
+def say_bye():
+    return "<h1>Bye my friend</h1>"
+
 
 if __name__ == "__main__":
     app.run()
