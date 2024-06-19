538da38d3f31a70e7dbfe57ceda7ca32ae93915f
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-06-19 14:26:59 -0600

    Add style to blog

--- Day57/server.py
+++ Day57/server.py
@@ -26,10 +26,20 @@ def guess(name):
 
 @app.route("/blog")
 def get_blog():
+    current_year = datetime.datetime.today().year
     posts_url = "https://api.npoint.io/e89ec700ad8552c04598/"
     posts_request = requests.get(posts_url)
     posts_data = posts_request.json()
-    return render_template("blog.html", posts = posts_data)
+    return render_template("blog.html", posts = posts_data, year = current_year)
+
+@app.route("/blog/<int:id>")
+def show_post(id):
+    current_year = datetime.datetime.today().year
+    new_id = id - 1
+    post_url = f"https://api.npoint.io/e89ec700ad8552c04598/{new_id}"
+    post_request = requests.get(post_url)
+    post_data = post_request.json()
+    return render_template("show.html", post = post_data, year = current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
