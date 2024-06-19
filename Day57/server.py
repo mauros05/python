b4c07a20ba3fe8da0eaeddde6af37708a8685164
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-06-18 18:27:03 -0600

    Add date

--- Day57/server.py
+++ Day57/server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import random
+import datetime
 
 app =  Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.today().year
+    return render_template("index.html", num = random_number, year = current_year)
 
 
 if __name__ == "__main__":
