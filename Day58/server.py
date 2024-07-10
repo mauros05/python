a34880957ef14c6711151db80e3c04c1f0ee5721
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-07-09 21:17:52 -0600

    Add login validations

--- Day58/server.py
+++ Day58/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = StringField(label='email', validators=[DataRequired()])
     password = PasswordField(label='password', validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
 
 app = Flask(__name__)
 app.secret_key = "something-i-dont-know"
@@ -14,9 +15,14 @@ app.secret_key = "something-i-dont-know"
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
    login_form = LoginForm()
+   if login_form.validate_on_submit():
+       if login_form.email.data == "admin@email.com" and login_form.password.data == "123456":
+           return render_template("success.html")
+       else:
+           return render_template("denied.html")
    return render_template("login.html", form=login_form)
 
 
