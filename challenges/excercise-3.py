00724d956e3aa6d9a16ca3d15091ca9e30a4723d
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-11-04 23:23:41 -0600

    Third excercise completed

---
+++ challenges/excercise-3.py
@@ -0,0 +1,16 @@
+# BMI Calculator
+
+# The body mass index (BMI) is a measure used in medicine to see if someone is underweight or 
+# overweight. This is the formula used to calculate it:
+
+
+# bmi is equal to the person's weight divided by the person's height squared.
+
+# Convert this sentence into code on line 6.
+
+weight = 92.6
+height = 1.81
+
+bmi = weight / (height**2)
+
+print(bmi)
