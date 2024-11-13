4325f0d1cece5519a2c437470bb7363f0767904d
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-11-12 23:07:10 -0600

    grades challege completed

---
+++ challenges/excercise-8.py
@@ -0,0 +1,45 @@
+# You have access to a database of student_scores in the format of a dictionary. The keys in 
+# student_scores are the names of the students and the values are their exam scores. 
+
+
+# Write a program that converts their scores to grades.
+
+# By the end of your program, you should have a new dictionary called student_grades that 
+# should contain student names as keys and their assessed grades for values. 
+
+
+# The final version of the student_grades dictionary will be checked. 
+# **DO NOT** modify lines 1-7 to change the existing student_scores dictionary. 
+
+
+# This is the scoring criteria: 
+
+# - Scores 91 - 100: Grade = "Outstanding" 
+
+# - Scores 81 - 90: Grade = "Exceeds Expectations" 
+
+# - Scores 71 - 80: Grade = "Acceptable" 
+
+# - Scores 70 or lower: Grade = "Fail" 
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for student, score in student_scores.items():
+    if score <= 70:
+        student_grades[student] = "Fail"
+    elif score >= 71 and score <= 80:
+        student_grades[student] = "Acceptable"
+    elif score >= 81 and score <= 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 91 and score <= 100:
+        student_grades[student] = "Outstanding"
+
+print(student_grades)
